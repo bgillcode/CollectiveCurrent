0dbf446188192e073b76a8a64173f8f03d7799f5
B Gill <w6u03@students.keele.ac.uk>
2021-03-10 14:54:05 +0000

    Add argument parsing

--- collective_current.py
+++ collective_current.py
@@ -7,31 +7,52 @@ from urllib.request import Request, urlopen
 from urllib.parse import urlparse
 import urllib.parse
 import requests
-import threading
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--url", "-u", help="Set URL to retrieve the images from.")
+
+urlInputted = ''
+
+# Read arguments from the command line
+args = parser.parse_args()
+
+checkURLEntered = False
+
+# Check for --url
+if args.url:
+    if str(args.url).strip().find("http") != -1:
+        checkURLEntered = True
+    urlInputted = str(args.url)
 
 rangeStart = 1
 rangeEnd = 2
 
-for i in range(rangeStart, rangeEnd):
-    # Pass in the page that you want
-    url = "https://manganelo.com/chapter/bi918289/chapter_1"
-    req = Request(url, headers={'User-agent': 'Mozilla/5.0'})
-    page = urlopen(req).read()
-    soup = BeautifulSoup(page, 'html.parser')
+if checkURLEntered == False:
+    print('Please try again and enter a valid URL to retrieve the images from')
+    sys.exit()
 
-    textGottenContent = soup.find(class_="container-chapter-reader").find_all('img')
+else:
+    for i in range(rangeStart, rangeEnd):
+        url = urlInputted
+        req = Request(url, headers={'User-agent': 'Mozilla/5.0'})
+        page = urlopen(req).read()
+        soup = BeautifulSoup(page, 'html.parser')
 
-    for line in textGottenContent:
-        domain = 'https://' + urllib.parse.urlparse(line['src']).netloc + urllib.parse.urlparse(line['src']).path
+        textGottenContent = soup.find(class_="container-chapter-reader").find_all('img')
 
-        print(domain)
-        r = requests.get(domain, headers={'Accept': 'image/png,image/svg+xml,image/*;q=0.8,video/*;q=0.8,*/*;q=0.5', \
-        'Accept-Encoding': 'gzip, deflate, br', \
-        'referer': 'https://manganelo.com/'})
-        print(r)
+        for line in textGottenContent:
+            domain = 'https://' + urllib.parse.urlparse(line['src']).netloc + urllib.parse.urlparse(line['src']).path
 
-        a = urlparse(line['src'])
-        filenameGotten = os.path.basename(a.path)
-        filenameNumber = filenameGotten.split('.')[0].zfill(3) + '.' + filenameGotten.split('.')[1]
-        print(filenameNumber)
-        open(str(filenameGotten).zfill(3), 'wb').write(r.content)
+            print(domain)
+            r = requests.get(domain, headers={'Accept': 'image/png,image/svg+xml,image/*;q=0.8,video/*;q=0.8,*/*;q=0.5', \
+            'Accept-Encoding': 'gzip, deflate, br', \
+            'referer': 'https://manganelo.com/'})
+            print(r)
+
+            a = urlparse(line['src'])
+            filenameGotten = os.path.basename(a.path)
+            filenameNumber = filenameGotten.split('.')[0].zfill(3) + '.' + filenameGotten.split('.')[1]
+            print(filenameNumber)
+            open(str(filenameGotten).zfill(3), 'wb').write(r.content)
